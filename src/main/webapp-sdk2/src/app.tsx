0989e7ab43d08472687e3fcb142968bc404b6ea9
seregamorph <serega.morph@gmail.com>
2022-04-20 14:28:15 +0200

    sdk2

--- src/main/webapp-sdk2/src/app.tsx
+++ src/main/webapp-sdk2/src/app.tsx
@@ -1,43 +1,136 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import axios, {AxiosResponse} from 'axios'
 
-async function init() {
-  const stickyNote = await miro.board.createStickyNote({
-    content: 'Hello, World!',
-  });
-
-  await miro.board.viewport.zoomTo(stickyNote);
-}
+import './styles.css'
 
 function App() {
-  React.useEffect(() => {
-    init();
-  }, []);
+    const [boardState, setBoardState] = React.useState({
+        title: ''
+    })
 
-  return (
-    <div className="grid wrapper">
-      <div className="cs1 ce12">
-        <img src="/src/assets/congratulations.png" alt="" />
-      </div>
-      <div className="cs1 ce12">
-        <h1>Congratulations!</h1>
-        <p>You've just created your first Miro app!</p>
-        <p>
-          To explore more and build your own app, see the Miro Developer
-          Platform documentation.
-        </p>
-      </div>
-      <div className="cs1 ce12">
-        <a
-          className="button button-primary"
-          target="_blank"
-          href="https://developers.miro.com/docs/welcome"
-        >
-          Read the documentation
-        </a>
-      </div>
-    </div>
-  );
+    const [authState, setAuthState] = React.useState({
+        authorized: ''
+    })
+
+    function resetAuthState() {
+        setAuthState({authorized: ""})
+    }
+
+    // async function updateAuthState() {
+    //     let authorized = await miro.isAuthorized()
+    //     setAuthState({authorized: authorized ? "✅" : "❌"})
+    //     return authorized
+    // }
+
+    async function getBoardId() {
+        const boardInfo = await miro.board.getInfo()
+        setBoardState({title: boardInfo.id})
+    }
+
+    // async function getScopes() {
+    //     const scopes = await miro.currentUser.getScopes()
+    //     console.error(`Scopes: ${scopes}`)
+    //     await miro.showNotification(`Scopes: ${scopes}`)
+    // }
+
+    // async function getClientId() {
+    //     const clientId = miro.board.getClientId()
+    //     await miro.showNotification(`Client id : ${clientId}`)
+    // }
+
+    // async function getToken() {
+    //     const token = await miro.getToken()
+    //     await updateAuthState()
+    //     console.error(`You should not use this method!!! Token is "${token}"`)
+    //     alert(`You should not use this method!!! Token is "${token}"`)
+    // }
+
+    async function getIdToken() {
+        const token = await miro.board.getIdToken()
+        //await updateAuthState()
+        console.error(`Id token is "${token}"`)
+        //await miro.showNotification(`Id token is ${token}`)
+    }
+
+    // async function isAuthorized() {
+    //     const isAuthorized = await updateAuthState()
+    //     console.error(`Is authorized: "${isAuthorized}"`)
+    //     await miro.showNotification(`Is authorized ${isAuthorized}`)
+    // }
+
+    // async function requestAuthorization() {
+    //     const redirectUrl = new URL(window.location.href)
+    //     redirectUrl.pathname = "/install"
+    //
+    //     await miro.board.requestAuthorization({
+    //         state: "test-state",
+    //         redirect_uri: redirectUrl.href
+    //     })
+    //     await updateAuthState()
+    //     console.error(`Authorization requested`)
+    // }
+
+    async function getSelfUser() {
+        const backendUrl = new URL(window.location.href)
+        backendUrl.pathname = "/get-self-user"
+
+        const token = await miro.board.getIdToken()
+        resetAuthState()
+        axios.get(backendUrl.href,
+            {
+                headers: {
+                    "X-Miro-Token": token
+                }
+            })
+            .then((response: AxiosResponse) => {
+                console.error(`callBackend: "${response.data.name}"`)
+                //miro.showNotification(`callBackend: user name="${response.data.name}"`)
+            })
+            .catch((error) => {
+                let message = error.message
+                if (error.response) {
+                    message = JSON.stringify(error.response.data)
+                } else if (error.request) {
+                    message = "request: " + error.request
+                }
+
+                console.error(`callBackend error: "${message}"`)
+                alert(`callBackend: error "${message}"`)
+            });
+    }
+
+    return (
+        <div className="container centered">
+            <div>Board title is: {boardState.title}</div>
+            <br/>
+            <button onClick={() => getBoardId()}>Get board title</button>
+            {/*<br/>*/}
+            {/*<button onClick={() => deleteAllContent()}>Delete all content</button>*/}
+            {/*<br/>*/}
+            {/*<button onClick={() => getScopes()}>getScopes</button>*/}
+            <br/>
+            <br/>
+            <div>Authorized: {authState.authorized}</div>
+            {/*<br/>*/}
+            {/*<button onClick={() => getClientId()}>getClientId</button>*/}
+            {/*<br/>*/}
+            {/*<button onClick={() => getToken()}><del>getToken</del></button>*/}
+            <br/>
+            <button onClick={() => getIdToken()}>getIdToken</button>
+            {/*<br/>*/}
+            {/*<button onClick={() => isAuthorized()}>isAuthorized</button>*/}
+            {/*<br/>*/}
+            {/*<button onClick={() => authorize()}><del>authorize</del></button>*/}
+            {/*<br/>*/}
+            {/*<button onClick={() => requestAuthorization()}>requestAuthorization</button>*/}
+            <br/>
+            <br/>
+            <button onClick={() => getSelfUser()}>call backend: getSelfUser</button>
+            {/*<br/>*/}
+            {/*<button onClick={() => createBoard()}>call backend: create new Board in account</button>*/}
+        </div>
+    )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'))
