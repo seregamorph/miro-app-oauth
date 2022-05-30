c2761400d5932def8d8a04f183ac248ac1ba8686
seregamorph <serega.morph@gmail.com>
2022-05-30 17:37:15 +0200

    support token region

--- src/main/kotlin/com/miro/miroappoauth/model/TokenRecord.kt
+++ src/main/kotlin/com/miro/miroappoauth/model/TokenRecord.kt
@@ -2,6 +2,7 @@ package com.miro.miroappoauth.model
 
 import java.net.URI
 import java.time.OffsetDateTime
+import java.util.*
 
 /**
  * For UI view.
@@ -21,4 +22,19 @@ data class TokenRecord(
     val checkValidUrl: URI,
     val refreshUrl: URI?,
     val revokeUrl: URI
-)
+) {
+    fun getRegion(): String {
+        val parts = accessTokenValue.split('_')
+        return if (parts.size < 2) {
+            ""
+        } else {
+            val regionEncoded = parts[0]
+            try {
+                val decoded: ByteArray = Base64.getDecoder().decode(regionEncoded)
+                String(decoded)
+            } catch (e: IllegalArgumentException) {
+                ""
+            }
+        }
+    }
+}
