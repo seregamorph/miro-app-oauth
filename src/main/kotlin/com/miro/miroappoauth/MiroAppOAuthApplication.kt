7948e10fe18219785caca7fac45aebe83c6c6133
seregamorph <serega.morph@gmail.com>
2023-08-26 17:16:20 +0200

    spring-boot 3.2.0-M2

--- src/main/kotlin/com/miro/miroappoauth/MiroAppOAuthApplication.kt
+++ src/main/kotlin/com/miro/miroappoauth/MiroAppOAuthApplication.kt
@@ -1,15 +1,24 @@
 package com.miro.miroappoauth
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.miro.miroappoauth.config.AppProperties
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.actuate.endpoint.jackson.EndpointObjectMapper
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties
 import org.springframework.boot.autoconfigure.web.ServerProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent
+import org.springframework.cglib.core.ReflectUtils
+import org.springframework.context.annotation.Bean
 import org.springframework.context.event.EventListener
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+import java.io.File
+import java.io.FileOutputStream
 
 @EnableConfigurationProperties(AppProperties::class)
 @SpringBootApplication
@@ -40,8 +49,34 @@ class MiroAppOAuthApplication {
             }
         }
     }
+
+    @Bean
+    fun endpointObjectMapper(): EndpointObjectMapper {
+        val objectMapper = Jackson2ObjectMapperBuilder.json()
+            .featuresToDisable(
+                SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
+                SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS
+            )
+            .serializationInclusion(JsonInclude.Include.NON_NULL)
+            .featuresToEnable(SerializationFeature.INDENT_OUTPUT)
+            .build<ObjectMapper>()
+        return EndpointObjectMapper { objectMapper }
+    }
 }
 
 fun main(args: Array<String>) {
+    if (false) {
+        initGeneratedClassHandler("cglib")
+    }
     runApplication<MiroAppOAuthApplication>(*args)
+}
+
+fun initGeneratedClassHandler(targetPath: String?) {
+    val dir = File(targetPath)
+    dir.mkdirs()
+    ReflectUtils.setGeneratedClassHandler { className, classContent ->
+        FileOutputStream(File(dir, "$className.class")).use {
+                out -> out.write(classContent)
+        }
+    }
 }
